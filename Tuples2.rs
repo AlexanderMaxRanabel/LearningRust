8e398c35bb6ee1998dfb23ef86ca695231c2ac77
AlexanderMaxRanabel <93342430+AlexanderMaxRanabel@users.noreply.github.com>
2022-12-15 16:08:58 +0300

    Create Tuples2.rs

---
+++ Tuples2.rs
@@ -0,0 +1,8 @@
+fn main() {
+  let tup1 = (128, 54, "It Starting To Get Rusty!");
+  let (a, b, c) = tup1;    
+    
+    println!("a is {}", a);
+    println!("b is {}", b);
+    println!("c is {}", c);
+}
